7e46dc861468d99ec6eb4d980f657624a0e1d8b9
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-23 10:32:44 +0300

    ch: create project files

---
+++ src/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.db.models.signals import post_save
+
+
+# Create your models here.
+class UserProfile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(max_length=500, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(max_length=20, blank=True)
+    picture = CloudinaryField('image', blank=True)
+    
+    
+    def __str__(self):
+        return self.user.username
+    
+    # user profile creation
+    def create_profile( sender, **kwargs):
+        user = kwargs['instance']
+        if kwargs['created']:
+            user_profile = UserProfile(user=user)
+            user_profile.save()
+    post_save.connect(create_profile, sender=User)   
