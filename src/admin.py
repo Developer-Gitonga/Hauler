564d8c92aa09dda9bdb5e3062ae95dca03b96500
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-24 00:20:30 +0300

    ft: register profile model to admin

--- src/admin.py
+++ src/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from src.models import UserProfile
 
 # Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ['user']}),
+        ('Personal Info', {'fields': ['bio', 'phone', 'picture']}),
+    )
+    
+admin.site.register(UserProfile, ProfileAdmin)
