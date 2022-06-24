a301affd8281919ae5ab7c0728614ab27ae5e538
Willbeckh <wileyhndirangu04@gmail.com>
2022-06-24 10:15:47 +0300

    initial heroku deploy

--- public/settings.py
+++ public/settings.py
@@ -17,6 +17,7 @@ import cloudinary.api
 import dj_database_url
 import cloudinary.uploader
 from decouple import config, Csv
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -166,3 +167,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
